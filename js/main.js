835cd18c4ffc931216146ebdc18b6380aacf2c0a
graphic <graphic@WFD228-36m.local>
2020-01-30 13:07:46 -0500

    ui mods complete

--- js/main.js
+++ js/main.js
@@ -1,31 +1,36 @@
-// Capture the .signin click
+// ****** DOM REFERENCES ******
+let $modal =  document.querySelector('.modal')
+let $user =   document.querySelector('#user')
+let $pass =   document.querySelector('#pass')
+let $signin = document.querySelector('.signin')
+let $close =  document.querySelector('.close')
+let $submit = document.querySelector('.submit')
 
 
+// ****** UI MODIFICATIONS ******
 // Display the .modal
-
-
-// Capture the .close click
-
+$modal.style.display = `block`
 
 // Remove the .modal
-
-
-// Capture the .click submit
-
+$modal.style.display = `none`
 
 // Add .error to #user
-
+$user.classList.add(`error`)
 
 // Add .error to #pass
-
-
-// Capture the focus of #user
-
+$pass.classList.add(`error`)
 
 // Remove .error from #user
-
-
-// Capture the focus of #pass
-
+$user.classList.remove(`error`)
 
 // Remove .error from #pass
+$pass.classList.remove(`error`)
+
+
+
+// ****** INTERACTIVE EVENT LISTENERS ******
+// Capture the .signin click
+// Capture the .close click
+// Capture the .submit click
+// Capture the focus of #user
+// Capture the focus of #pass
