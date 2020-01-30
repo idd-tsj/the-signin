a3a1578655f8f03f01892604f8a05a5de102c063
graphic <graphic@WFD228-36m.local>
2020-01-30 12:50:58 -0500

    Outlining the step

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// Capture the .signin click
+
+
+// Display the .modal
+
+
+// Capture the .close click
+
+
+// Remove the .modal
+
+
+// Capture the .click submit
+
+
+// Add .error to #user
+
+
+// Add .error to #pass
+
+
+// Capture the focus of #user
+
+
+// Remove .error from #user
+
+
+// Capture the focus of #pass
+
+
+// Remove .error from #pass
