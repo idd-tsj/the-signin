33efe9095f314072b3d499bf7fb032544841b677
graphic <graphic@WFD228-36m.local>
2020-01-30 13:27:19 -0500

    Part 1 complete

--- js/main.js
+++ js/main.js
@@ -8,38 +8,41 @@ let $submit = document.querySelector('.submit')
 
 
 // ****** UI MODIFICATIONS ******
-// Display the .modal
-$modal.style.display = `block`
-
-// Remove the .modal
-$modal.style.display = `none`
-
-// Add .error to #user
-$user.classList.add(`error`)
-
-// Add .error to #pass
-$pass.classList.add(`error`)
-
-// Remove .error from #user
-$user.classList.remove(`error`)
-
-// Remove .error from #pass
-$pass.classList.remove(`error`)
-
-
-
 // ****** INTERACTIVE EVENT LISTENERS ******
+
 // Capture the .signin click
-$signin.addEventListener('click', event => { console.log('click') })
+$signin.addEventListener('click', event => { 
+  // Display the .modal
+  $modal.style.display = `block`
+})
+
 
 // Capture the .close click
-$close.addEventListener('click', event => { console.log('click') })
+$close.addEventListener('click', event => { 
+  // Remove the .modal
+  $modal.style.display = `none`
+})
+
 
 // Capture the .submit click
-$submit.addEventListener('click', event => { console.log('click') })
+$submit.addEventListener('click', event => { 
+  // Add .error to #user
+  $user.classList.add(`error`)
+
+  // Add .error to #pass
+  $pass.classList.add(`error`)
+})
+
 
 // Capture the focus of #user
-$user.addEventListener('focus', event => { console.log('focus') })
+$user.addEventListener('focus', event => { 
+  // Remove .error from #user
+  $user.classList.remove(`error`)
+})
+
 
 // Capture the focus of #pass
-$pass.addEventListener('focus', event => { console.log('focus') })
+$pass.addEventListener('focus', event => {
+  // Remove .error from #pass
+  $pass.classList.remove(`error`)
+})
