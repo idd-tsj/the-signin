78b7fb9d91ed95b483da50ff11d3deb4ec262e18
graphic <graphic@WFD228-36m.local>
2020-01-31 08:47:14 -0500

    Sorting by type

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// ****** UI CHANGES *******
+// Change the display style to block for the .modal
+// Change the display style to none for the .modal
+// Add .error class to #user
+// Add .error class to #pass
+// Remove .error class from #user
+// Remove .error class from #pass
+
+// ****** EVENT LISTENERS *******
+// Capture .signin click
+// Capture .close click
+// Capture .submit click
+// Capture #user focus
+// Capture #pass focus
