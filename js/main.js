2f51261e5126d2e8dd680852a0e04898f63b68f8
graphic <graphic@WFD228-36m.local>
2020-01-31 08:54:39 -0500

    store references

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+// ****** STORE ALL DOM REFERENCES ******
+let $modal =  document.querySelector('.modal')
+let $user =   document.querySelector('#user')
+let $pass =   document.querySelector('#pass')
+let $signin = document.querySelector('.signin')
+let $close =  document.querySelector('.close')
+let $click =  document.querySelector('.click')
+
 // ****** UI CHANGES *******
 // Change the display style to block for the .modal
 // Change the display style to none for the .modal
