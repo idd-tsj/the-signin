03a089f52116669c9d4acf53017320511f06b0c8
graphic <graphic@WFD228-36m.local>
2020-01-31 09:11:43 -0500

    UI changes

--- js/main.js
+++ js/main.js
@@ -4,15 +4,27 @@ let $user =   document.querySelector('#user')
 let $pass =   document.querySelector('#pass')
 let $signin = document.querySelector('.signin')
 let $close =  document.querySelector('.close')
-let $click =  document.querySelector('.click')
+let $submit = document.querySelector('.submit')
 
 // ****** UI CHANGES *******
 // Change the display style to block for the .modal
+$modal.style.display = 'block'
+
 // Change the display style to none for the .modal
+$modal.style.display = 'none'
+
 // Add .error class to #user
+$user.classList.add('error')
+
 // Add .error class to #pass
+$pass.classList.add('error')
+
 // Remove .error class from #user
+$user.classList.remove('error')
+
 // Remove .error class from #pass
+$pass.classList.remove('error')
+
 
 // ****** EVENT LISTENERS *******
 // Capture .signin click
