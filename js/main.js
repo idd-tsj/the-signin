38d6af2f7618343f7ab308aa06fa7aa780918d47
graphic <graphic@WFD228-36m.local>
2020-01-31 09:40:14 -0500

    Complete Part 1

--- js/main.js
+++ js/main.js
@@ -6,38 +6,41 @@ let $signin = document.querySelector('.signin')
 let $close =  document.querySelector('.close')
 let $submit = document.querySelector('.submit')
 
+
 // ****** UI CHANGES *******
-// Change the display style to block for the .modal
-$modal.style.display = 'block'
-
-// Change the display style to none for the .modal
-$modal.style.display = 'none'
-
-// Add .error class to #user
-$user.classList.add('error')
-
-// Add .error class to #pass
-$pass.classList.add('error')
-
-// Remove .error class from #user
-$user.classList.remove('error')
-
-// Remove .error class from #pass
-$pass.classList.remove('error')
-
-
 // ****** EVENT LISTENERS *******
 // Capture .signin click
-$signin.addEventListener('click', event => { console.log('click') })
+$signin.addEventListener('click', event => { 
+  // Change the display style to block for the .modal
+  $modal.style.display = 'block'
+})
 
 // Capture .close click
-$close.addEventListener('click', event => { console.log('click') })
+$close.addEventListener('click', event => {
+  // Change the display style to none for the .modal
+  $modal.style.display = 'none'
+})
 
 // Capture .submit click
-$submit.addEventListener('click', event => { console.log('click') })
+$submit.addEventListener('click', event => {
+  // Add .error class to #user
+  $user.classList.add('error')
+
+  // Add .error class to #pass
+  $pass.classList.add('error')
+})
 
 // Capture #user focus
-$user.addEventListener('focus', event => { console.log('focus') })
+$user.addEventListener('focus', event => {
+  // Remove .error class from #user
+  $user.classList.remove('error')
+})
 
 // Capture #pass focus
-$pass.addEventListener('focus', event => { console.log('focus') })
+$pass.addEventListener('focus', event => {
+  // Remove .error class from #pass
+  $pass.classList.remove('error')
+})
+
+
+
